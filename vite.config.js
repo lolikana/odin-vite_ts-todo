233c3ca08a11293f73c8cca79a342e83d68af1a9
Lolikana <thomas.guiart@gmail.com>
2023-06-21 14:59:53 +0900

    install jsdom and testing library

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
@@ -5,10 +9,26 @@ export default defineConfig({
     outDir: 'dist',
     sourcemap: 'inline',
     minify: 'esbuild',
-    manifest: true
+    manifest: true,
+    emptyOutDir: true,
+    copyPublicDir: true
+  },
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, './src')
+      },
+      {
+        find: '@__tests__',
+        replacement: path.resolve(__dirname, './__tests__')
+      }
+    ]
   },
   test: {
-    globals: true
+    globals: true,
+    environment: 'jsdom',
+    exclude: ['node_modules', './__tests__/renderIndexHTML.ts']
   },
   server: {
     port: 4000
