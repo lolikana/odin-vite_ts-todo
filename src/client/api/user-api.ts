aa181bd99bf27ab036a147c380f2c6851f0149b3
Lolikana <thomas.guiart@gmail.com>
2023-06-30 11:53:08 +0900

    change error message

--- src/client/api/user-api.ts
+++ src/client/api/user-api.ts
@@ -5,7 +5,7 @@ import { isProduction, path } from '.';
 export const fetchUser = async (): Promise<IUser> => {
   const res = await fetch(`${!isProduction ? path : ''}/current-user`);
   if (!res.ok) {
-    throw new Error('Failed to retrieve todos from server');
+    throw new Error('No user found from server');
   }
   return res.json() as unknown as IUser;
 };
