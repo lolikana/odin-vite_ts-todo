2da65f3e42d54617a9533e82dec04469d33079a1
Lolikana <thomas.guiart@gmail.com>
2023-07-03 14:04:56 +0900

    comment secure cookie

--- src/server/main.ts
+++ src/server/main.ts
@@ -50,7 +50,7 @@ const sessionConfig = {
   saveUninitialized: true,
   cookie: {
     HttpOnly: true,
-    secure: true,
+    // secure: true,
     expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
     maxAge: 1000 * 60 * 60 * 24 * 7
   }
