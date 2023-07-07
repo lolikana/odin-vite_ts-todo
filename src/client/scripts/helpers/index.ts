33f5d3494bb0138fb8942575f4827a7e7fbf7e5c
Lolikana <thomas.guiart@gmail.com>
2023-07-07 11:29:23 +0900

    format date function for default value

--- src/client/scripts/helpers/index.ts
+++ src/client/scripts/helpers/index.ts
@@ -91,3 +91,11 @@ export function findMatchingTodos(
   }
   return matchingTodos;
 }
+
+export function formatDate(date: Date) {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+  const day = String(date.getDate()).padStart(2, '0');
+
+  return `${year}-${month}-${day}`;
+}
