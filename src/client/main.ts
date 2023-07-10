36140de6b6c0cd0d4710a7a5271310fcffb076db
Lolikana <thomas.guiart@gmail.com>
2023-07-10 15:01:04 +0900

    remove flash msg when click window

--- src/client/main.ts
+++ src/client/main.ts
@@ -6,3 +6,9 @@ import { setCsrfToken } from './scripts/helpers/getCsrfToken';
 
 setCsrfToken().catch(err => console.log(err));
 setFlashMsg().catch(err => console.log(err));
+
+window.addEventListener('click', () => {
+  const flashMsg = document.getElementById('connect-flash') as HTMLDivElement;
+  flashMsg.setAttribute('aria-hidden', 'true');
+  flashMsg.textContent = '';
+});
